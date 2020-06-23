37f1daddee51645e63aaeebe365574b1980247cb
Ahmed <ahmedhijazi94@gmail.com>
2020-06-23 14:35:01 -0300

    finalizando

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface GetTransactions {
+  transactions: Array<Transaction>;
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,43 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): GetTransactions {
+    return {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactions = this.transactions.filter(transaction => {
+      return transaction.type === 'income';
+    });
+
+    const totalIncomeTransactions = incomeTransactions.reduce(
+      (prevVal, elem) => prevVal + elem.value,
+      0,
+    );
+
+    const outcomeTransactions = this.transactions.filter(transaction => {
+      return transaction.type === 'outcome';
+    });
+
+    const totaloutcomeTransactions = outcomeTransactions.reduce(
+      (prevVal, elem) => prevVal + elem.value,
+      0,
+    );
+
+    return {
+      income: totalIncomeTransactions,
+      outcome: totaloutcomeTransactions,
+      total: totalIncomeTransactions - totaloutcomeTransactions,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
